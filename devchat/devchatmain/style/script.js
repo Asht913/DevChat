e8283624bb5648993461ac1e360e1b15ee2740b2
Asht913 <93267656+Asht913@users.noreply.github.com>
2021-12-18 13:07:42 -0600

    Blog Section
    Added content to the blog section

--- devchat/devchatmain/style/script.js
+++ devchat/devchatmain/style/script.js
@@ -15,3 +15,20 @@ if(pswd1 != pswd2) {
 function swapStyleSheet(sheet) {
     document.getElementById('pagestyle').setAttribute('href', sheet);
 }
+
+//event handlers
+
+function addGreet(event) {
+    event.preventDefault();
+    const greet = devBlog.greet.value;
+    const newGreetLi = document.createElement('li');
+    const newGreetAvatar = document.createElement('div');
+    newGreetAvatar.className="avatar";
+    const newGreetText= document.createElement('span');
+    newGreetText.innerText = greet;
+    const greets = documt.getElementById('greets').getElementsByTagName('ul')[0];
+    const newerGreetLi = greets.appendChild(newGreetLi);
+    newerGreetLi.appendChild(newGreetAvatar);
+    newerGreetLi.appendChild(newGreetText);
+    devBlog.greet.value = "";
+}
