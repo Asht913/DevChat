e09dd6b0188c003b529f9ba7161bb85f86f3bd9b
Asht913 <93267656+Asht913@users.noreply.github.com>
2021-12-24 01:08:20 -0600

    Add files via upload
    Changed the format of website.
    Updated the register.html file. 
    Made the website more responsive across the tablet and desktop.
    Updated the chat box.
    Started on database for blog post. 
    Added external links to display (youtube, gifs).

--- devchat/devchatmain/style/script.js
+++ devchat/devchatmain/style/script.js
@@ -14,33 +14,24 @@ if(pswd1 != pswd2) {
 //Changes Theme from DarkMode to LightMode
 function swapStyleSheet() {
     let themeSwap = document.getElementsByTagName('link')[0]; 
-    let imgSwap = document.getElementById('ldimg');
+    let imgSwap = document.getElementById('ldimg');   // calls for new image on
     if(themeSwap.getAttribute('href') == 'darkmode.css')
     { 
         themeSwap.setAttribute('href' , 'lightmode.css')
-        imgSwap.src = '/devchatmain/images/Darkmode.png'
+        imgSwap.src = '/devchatmain/images/Darkmode.png' 
         
     } 
     else{
         themeSwap.setAttribute('href' , 'darkmode.css')
         imgSwap.src = '/devchatmain/images/Lightmode.png'
         
+
     }
 }
 
-//event handlers
-
-function addGreet(event) {
-    event.preventDefault();
-    const greet = devBlog.greet.value;
-    const newGreetLi = document.createElement('li');
-    const newGreetAvatar = document.createElement('div');
-    newGreetAvatar.className="avatar";
-    const newGreetText= document.createElement('span');
-    newGreetText.innerText = greet;
-    const greets = document.getElementById('greets').getElementsByTagName('ul')[0];
-    const newerGreetLi = greets.appendChild(newGreetLi);
-    newerGreetLi.appendChild(newGreetAvatar);
-    newerGreetLi.appendChild(newGreetText);
-    devBlog.greet.value = "";
+function toggleblur(){
+    document.getElementsById("test").style.color = "blue";
+    
 }
+
+    
